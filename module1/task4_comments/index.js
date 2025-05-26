ccb98b21cf5d42f0b47b9da26801b38369d8f81f
Artem Sushko <artem.sushko.98@gmail.com>
2025-05-26 20:53:34 +0200

    Work con variables using const and let

--- module1/task4_comments/index.js
+++ module1/task4_comments/index.js
@@ -1,5 +1,15 @@
 // «JavaScript-код с созданием переменных»
+const myName = 'Artem';
+const mySurname = 'Sushko';
+const myFavoriteDrink = 'Water';
+const myFavoriteAnimal = 'Cat';
+const myFavoriteProgrammingLanguage = 'JavaScript';
 
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
 /* 
 Автор:
 Артем Сушко
---
+++ module1/task5_car/car.js
@@ -0,0 +1,21 @@
+const carName = 'Audi';
+let maxCarSpeed = 250;
+let carOwner = null;
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = 180;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 170;
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = 165;
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = 'Nick';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Max';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Ron';
+console.log('У машины изменился владелец', carOwner);
