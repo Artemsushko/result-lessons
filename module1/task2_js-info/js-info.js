3353abe614b4de8cfda1247e5e273522fdf0f930
Artem Sushko <artem.sushko.98@gmail.com>
2025-05-26 15:51:16 +0200

    Info about JS with alert()

---
+++ module1/task2_js-info/js-info.js
@@ -0,0 +1,11 @@
+const answers = [
+  'JavaScript разработал Брендан Эйх (Brendan Eich).',
+  'Язык был создан за 10 дней.',
+  'JavaScript появился в 1995 году.',
+  'JavaScript — это не Java.',
+  'Спецификация JavaScript называется ECMAScript.',
+];
+
+for (const answer of answers) {
+  alert(answer);
+}
