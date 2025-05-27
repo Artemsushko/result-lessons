e2c9495b0bc516a1723ca2c712ed3a9c2b1c4f0e
Artem Sushko <artem.sushko.98@gmail.com>
2025-05-27 12:42:41 +0200

    Create 8 data types

---
+++ module1/task7_data-types/data-types.js
@@ -0,0 +1,23 @@
+const age = 20;
+console.log('age', age, typeof age);
+
+const name = 'Alice';
+console.log('name', name, typeof name);
+
+const isStudent = true;
+console.log('isStudent', isStudent, typeof isStudent);
+
+const user = { id: 1, name: 'Alice' };
+console.log('user', user, typeof user);
+
+const id = Symbol('uniqueId');
+console.log('id', id, typeof id);
+
+const bigNumber = 123n;
+console.log('bigNumber', bigNumber, typeof bigNumber);
+
+const nothing = null;
+console.log('nothing', nothing, typeof nothing);
+
+const notDefined = undefined;
+console.log('notDefined', notDefined, typeof notDefined);
