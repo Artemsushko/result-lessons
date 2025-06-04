e6a3d1b090eb79daac175723b9dbe059682caf2c
Artem Sushko <artem.sushko.98@gmail.com>
2025-06-04 19:40:28 +0200

    fix bag when number is less then 0

--- module1/task26_function3/index.js
+++ module1/task26_function3/index.js
@@ -1,5 +1,5 @@
 function getDivisorsCount(num) {
-  if (!Number.isInteger(num)) {
+  if (!Number.isInteger(num) || num < 0) {
     alert(`${num} должен быть целым числом и больше нуля!`);
   }
 
@@ -19,7 +19,7 @@ function getDivisorsCount(num) {
   return `${amountOfDivisors} (divisors: ${divisors})`;
 }
 
-console.log(getDivisorsCount(4)); // Вернет 3 (делители - 1, 2, 4)
+console.log(getDivisorsCount(-2)); // Вернет 3 (делители - 1, 2, 4)
 console.log(getDivisorsCount(5)); // Вернет 2 (делители - 1, 5)
 console.log(getDivisorsCount(12)); // Вернет 6 (делители - 1, 2, 3, 4, 6, 12)
 console.log(getDivisorsCount(30)); // Вернет 8 (делители - 1, 2, 3, 5, 6, 10, 15, 30)
