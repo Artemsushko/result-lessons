2394c1c31319f4e8ebc7ef321c71c33a4b659693
Artem Sushko <artem.sushko.98@gmail.com>
2025-06-12 10:21:08 +0200

    Extra Date

---
+++ module1/extra15_JS-basic/script.js
@@ -0,0 +1,36 @@
+const peopleWithVisa = [
+  {
+    firstName: 'Stasia',
+    lastName: 'Ward',
+    criminalRecord: true,
+    passportExpiration: '19.06.2040',
+  },
+  {
+    firstName: 'Elliot',
+    lastName: 'Baker',
+    criminalRecord: false,
+    passportExpiration: '04.06.2041',
+  },
+  {
+    firstName: 'Leighann',
+    lastName: 'Scott',
+    criminalRecord: true,
+    passportExpiration: '31.07.2039',
+  },
+  {
+    firstName: 'Nick',
+    lastName: 'Pop',
+    criminalRecord: false,
+    passportExpiration: '31.12.2010',
+  },
+];
+
+function allowVisa(people) {
+  return people.filter(
+    ({ criminalRecord, passportExpiration }) =>
+      !criminalRecord && Date.now() < new Date(passportExpiration).getTime()
+  );
+}
+
+const result = allowVisa(peopleWithVisa);
+console.log('result', result);
