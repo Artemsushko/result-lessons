947f29fd1adcd3dfbd635722cf41c721ab475231
Artem Sushko <artem.sushko.98@gmail.com>
2025-06-30 14:13:04 +0200

    Small refactor

--- module1/extra19_JS-basic/script.js
+++ module1/extra19_JS-basic/script.js
@@ -10,50 +10,59 @@ class CustomSelect {
     const dropDown = document.createElement('div');
     dropDown.className = `select-dropdown select-dropdown--${this.id}`;
 
-    const dropDownBtn = document.createElement('button');
-    dropDownBtn.className = `select-dropdown__button select-dropdown__button--${this.id}`;
-    dropDownBtn.addEventListener('click', () => {
-      dropDownList.classList.toggle('active');
-    });
-
     const dropDownText = document.createElement('span');
     dropDownText.className = `select-dropdown__text select-dropdown__text--${this.id}`;
+    dropDownText.textContent = 'Выберите...';
 
     const dropDownList = document.createElement('ul');
     dropDownList.className = `select-dropdown__list select-dropdown__list--${this.id}`;
     dropDownList.addEventListener('click', (event) => {
       const { target } = event;
       if (target.classList.contains('select-dropdown__list-item')) {
-        const allListItem = document.querySelectorAll(
+        const allListItem = dropDownList.querySelectorAll(
           '.select-dropdown__list-item'
         );
         allListItem.forEach((item) => {
           item.classList.remove('selected');
         });
         target.classList.add('selected');
+        dropDownList.classList.toggle('active');
 
-        const currentOption = this.options[target.dataset.value - 1];
+        const currentOption = this.options.find(
+          (op) => op.value == target.dataset.value
+        );
         this.#currentSelectedOption = currentOption;
         dropDownText.textContent = currentOption.text;
       }
     });
 
+    const dropDownBtn = document.createElement('button');
+    dropDownBtn.className = `select-dropdown__button select-dropdown__button--${this.id}`;
+    dropDownBtn.addEventListener('click', () => {
+      dropDownList.classList.toggle('active');
+    });
+
     this.options.forEach((el) => {
-      const ojbValues = Object.values(el);
       const listItem = document.createElement('li');
       listItem.className = 'select-dropdown__list-item';
-      listItem.dataset.value = ojbValues[0];
-      listItem.textContent = ojbValues[1];
+      listItem.dataset.value = el.value;
+      listItem.textContent = el.text;
       dropDownList.append(listItem);
     });
 
     dropDownBtn.append(dropDownText);
     dropDown.append(dropDownBtn, dropDownList);
     container.append(dropDown);
+
+    document.addEventListener('click', (event) => {
+      if (!dropDown.contains(event.target)) {
+        dropDownList.classList.remove('active');
+      }
+    });
   }
 
   get selectedValue() {
-    return this.#currentSelectedOption;
+    return this.#currentSelectedOption?.value;
   }
 }
 
