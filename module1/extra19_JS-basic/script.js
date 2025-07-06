a8696606baf06364a613ceefb4d43815f9039e03
Artem Sushko <artem.sushko.98@gmail.com>
2025-07-06 15:53:54 +0300

    Split code into separate functions for better readability

--- module1/extra19_JS-basic/script.js
+++ module1/extra19_JS-basic/script.js
@@ -6,41 +6,46 @@ class CustomSelect {
     this.options = options;
   }
 
-  render(container) {
+  #handleOutsideClick(dropDownList, dropDown) {
+    const outsideClickListener = (event) => {
+      if (!dropDown.contains(event.target)) {
+        dropDownList.classList.remove('active');
+      }
+    };
+
+    document.addEventListener('click', outsideClickListener);
+  }
+
+  #handleListItem(dropDownList, dropDownBtn, dropDown, event) {
+    const { target } = event;
+
+    if (target.classList.contains('select-dropdown__list-item')) {
+      dropDownList
+        .querySelectorAll('.select-dropdown__list-item')
+        .forEach((item) => item.classList.remove('selected'));
+      target.classList.add('selected');
+      const currentOption = this.options.find(
+        (op) => op.value == target.dataset.value
+      );
+      this.#currentSelectedOption = currentOption;
+
+      dropDownBtn.textContent = currentOption.text;
+
+      dropDownList.classList.remove('active');
+
+      this.#handleOutsideClick(dropDownList, dropDown, dropDownBtn);
+    }
+  }
+
+  #createDropDown(dropDownBtn) {
     const dropDown = document.createElement('div');
     dropDown.className = `select-dropdown select-dropdown--${this.id}`;
 
     const dropDownText = document.createElement('span');
     dropDownText.className = `select-dropdown__text select-dropdown__text--${this.id}`;
-    dropDownText.textContent = 'Выберите...';
 
     const dropDownList = document.createElement('ul');
     dropDownList.className = `select-dropdown__list select-dropdown__list--${this.id}`;
-    dropDownList.addEventListener('click', (event) => {
-      const { target } = event;
-      if (target.classList.contains('select-dropdown__list-item')) {
-        const allListItem = dropDownList.querySelectorAll(
-          '.select-dropdown__list-item'
-        );
-        allListItem.forEach((item) => {
-          item.classList.remove('selected');
-        });
-        target.classList.add('selected');
-        dropDownList.classList.toggle('active');
-
-        const currentOption = this.options.find(
-          (op) => op.value == target.dataset.value
-        );
-        this.#currentSelectedOption = currentOption;
-        dropDownText.textContent = currentOption.text;
-      }
-    });
-
-    const dropDownBtn = document.createElement('button');
-    dropDownBtn.className = `select-dropdown__button select-dropdown__button--${this.id}`;
-    dropDownBtn.addEventListener('click', () => {
-      dropDownList.classList.toggle('active');
-    });
 
     this.options.forEach((el) => {
       const listItem = document.createElement('li');
@@ -50,15 +55,26 @@ class CustomSelect {
       dropDownList.append(listItem);
     });
 
-    dropDownBtn.append(dropDownText);
-    dropDown.append(dropDownBtn, dropDownList);
-    container.append(dropDown);
-
-    document.addEventListener('click', (event) => {
-      if (!dropDown.contains(event.target)) {
-        dropDownList.classList.remove('active');
-      }
+    dropDownList.addEventListener('click', (event) => {
+      this.#handleListItem(dropDownList, dropDownBtn, dropDown, event);
     });
+
+    dropDownBtn.className = `select-dropdown__button select-dropdown__button--${this.id}`;
+    dropDownBtn.textContent = 'Открыть меню';
+    dropDownBtn.addEventListener('click', () => {
+      dropDownList.classList.toggle('active');
+      this.#handleOutsideClick(dropDownList, dropDown, dropDownBtn);
+    });
+
+    dropDown.append(dropDownBtn, dropDownList);
+
+    return dropDown;
+  }
+
+  render(container) {
+    const dropDownBtn = document.createElement('button');
+    const dropDown = this.#createDropDown(dropDownBtn);
+    container.append(dropDown);
   }
 
   get selectedValue() {
