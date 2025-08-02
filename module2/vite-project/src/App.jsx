78da11e51bfe6fc873555b98bc5ec24b9229d891
Artem Sushko <artem.sushko.98@gmail.com>
2025-08-02 11:58:20 +0200

    Rewrite app using js instead of jsx

--- module2/vite-project/src/App.jsx
+++ module2/vite-project/src/App.jsx
@@ -1,44 +1,73 @@
-import { useState } from "react"; // декларативный: импорт хука, используем React API
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // императивный
   const [count, setCount] = useState(0);
 
-  return (
-    <>
-      {/* декларативный */}
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+  const countHandler = () => {
+    setCount(count + 1);
+  };
 
-      <h1>Vite + React</h1>
+  const viteImg = React.createElement("img", {
+    src: viteLogo,
+    className: "logo",
+    alt: "Vite Logo",
+  });
+  const viteLink = React.createElement("a", { target: "_blank" }, viteImg);
 
-      <div className="card">
-        {/* декларативный: кнопка и её внешний вид */}
-        <button onClick={() => setCount((count) => count + 1)}>
-          {/* императивный */}
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+  const reactImg = React.createElement("img", {
+    src: reactLogo,
+    className: "logo react",
+    alt: "React Logo",
+  });
+  const reactLink = React.createElement("a", { target: "_blank" }, reactImg);
+  const logosContainer = React.createElement("div", null, viteLink, reactLink);
 
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  const title = React.createElement("h1", null, "Vite + React");
 
-      {/* императивный */}
-      <p>{new Date().getFullYear()}</p>
-    </>
+  const countBtn = React.createElement(
+    "button",
+    { onClick: countHandler },
+    `count is ${count}`
+  );
+
+  const editHint = React.createElement(
+    "p",
+    null,
+    "Edit",
+    React.createElement("code", null, "src/App.jsx"),
+    "and save to test HMR"
+  );
+
+  const cardContainer = React.createElement(
+    "div",
+    { className: "card" },
+    countBtn,
+    editHint
+  );
+
+  const docsLink = React.createElement(
+    "p",
+    { className: "read-the-docs" },
+    " Click on the Vite and React logos to learn more"
+  );
+
+  const currentYear = React.createElement(
+    "p",
+    null,
+    `${new Date().getFullYear()}`
+  );
+
+  return React.createElement(
+    React.Fragment,
+    null,
+    logosContainer,
+    title,
+    cardContainer,
+    docsLink,
+    currentYear
   );
 }
 
